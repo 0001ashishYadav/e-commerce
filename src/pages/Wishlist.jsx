a484e94f1b887385abd1ba489de0fe9f94021576
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-01 19:27:24 +0530

    this commit i make wishlist page and add to routing for page

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import WishlistComponent from "../components/wishlist/WishlistComponent";
 
 const WishlistPage = () => {
-  return <section className="pt-16 max-w-7xl mx-auto ">Wshlist Page</section>;
+  return (
+    <section className="pt-18 max-w-7xl mx-auto ">
+      {/* wishlist */}
+
+      <WishlistComponent />
+    </section>
+  );
 };
 
 export default WishlistPage;
