b5794dec6184884ae824bb2fb2f3aa572d1a3d62
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-24 16:11:15 +0530

    make data file for store data offline

---
+++ src/data/cartData.js
@@ -0,0 +1,27 @@
+export const cartItems = [
+  {
+    img: "https://f.media-amazon.com/images/I/71zfpkr4bYL._SX522_.jpg",
+    title: "Bluetooth Headphones",
+    price: "1499",
+  },
+  {
+    img: "https://f.media-amazon.com/images/I/414rdyFGiGL._SX300_SY300_QL70_FMwebp_.jpg",
+    title: "Pigeon Inox bottle 900ml",
+    price: "179",
+  },
+  {
+    img: "https://f.media-amazon.com/images/I/51Oq79-eK-L._SY445_SX342_QL70_FMwebp_.jpg",
+    title: "One94Store 3D Galaxy Crystal Ball ",
+    price: "1499",
+  },
+  {
+    img: "https://f.media-amazon.com/images/I/31Xz-5Ua8HL._SY300_SX300_QL70_FMwebp_.jpg",
+    title: "ECOVACS DEEBOT Robot Vaccum",
+    price: "1,29,999",
+  },
+  {
+    img: "https://f.media-amazon.com/images/I/81yobRRV8nL._SX679_.jpg",
+    title: "Cookwell Bullet Mixer Grinder",
+    price: "2,048",
+  },
+];
