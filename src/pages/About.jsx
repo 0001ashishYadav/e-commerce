4694cb3145fa44dd1f5a4c11dd475d87c826d216
000ashishYadav <000ashishyadav2003@gmail.com>
2025-06-02 21:21:53 +0530

    use heroSection component in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import HeroSection from "../components/about/HeroSection";
 
 const AboutPage = () => {
-  return <section className="pt-18 max-w-7xl mx-auto">AboutPage</section>;
+  return (
+    <section className="pt-18 max-w-7xl mx-auto">
+      {/* our story section */}
+
+      <HeroSection />
+    </section>
+  );
 };
 
 export default AboutPage;
